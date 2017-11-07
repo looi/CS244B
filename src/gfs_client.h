aad82c280e4ea75927f6b7ae1e6df20fec73e12f
Ge <bian@google.com>
2017-11-07 01:07:09 -0800

    Add declarations of Client API functions

--- src/gfs_client.h
+++ src/gfs_client.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <set>
 
 #include <grpc++/grpc++.h>
 #include "gfs.grpc.pb.h"
@@ -13,12 +14,36 @@ class GFSClient {
   // from the server.
   std::string ClientServerPing(const std::string& user);
 
+  // Create a new file using absolute path. Returns true if success.
+  bool Create(const std::string& filename);
+
+  // Make a new directory using absolute path. Returns true if succeeded.
+  bool Mkdir(const std::string& path);
+
+  // List all files and directories under an absolute path.
+  std::set<std::string> List(const std::string& path);
+
+  // Delete a file (or a directory) given an absolute path. Returns true if
+  // succeeded.
+  bool Delete(const std::string& path);
+
+  // Fills the byte array with contents with off-set in the file. Returns the 
+  // number of bytes it reads.
+  int Read(char *buf, const std::string& filename, onst int offset);
+
+  // Writes the byte array content to the file with an off-set. Returns the
+  // number of bytes it reads.
+  bool Write(char *buf, const std::string& filename, onst int offset);
+  
+  // Appends the byte array to a file. Returns the off-set that the content resides in.
+  int Append(har *buf, const std::string& filename);
+
+private:
   // Client ReadChunk implementation
   std::string ReadChunk(const int chunkhandle);
 
   // Client WriteChunk implementation
   std::string WriteChunk(const int chunkhandle, const std::string data);
 
- private:
   std::unique_ptr<gfs::GFS::Stub> stub_;
 };
